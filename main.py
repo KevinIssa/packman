9bde85648164c0ac77edf7e996d6159e73aa0e7c
Linsfa <kevin.vandervaeren@ulb.be>
2022-02-09 17:01:26 +0100

    first function the one that read the files and interpret into a list of list type of matrices

--- main.py
+++ main.py
@@ -3,3 +3,15 @@ from turtle import fd, up, down, goto, Turtle
 #menfou test
 # lol > dota 2
 game_space = Turtle()
+
+
+def read_matrices(files):  # mooc project (lire matrices) @linsfa
+    """
+    going to read the file with the matrices
+    """
+    matrices_conversion = []
+    for row in open(files, encoding='utf-8'):
+        matrices_conversion.append([int(i) for i in row if i.isalnum()])
+    return matrices_conversion
+
+

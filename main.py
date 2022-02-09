bb66ecef8434b9401b70b27e18cae561963b3167
Astramast <azerqsdfazerqsdg@gmail.com>
2022-02-09 15:29:52 +0100

    créé une turtle pour dessiner l'epace de jeu (murs,etc)

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-from turtle import fd, up, down, goto
-# test
+from turtle import fd, up, down, goto, Turtle
 # on peut modifier le fichier directement sur Github en mode trkl
+
+game_space = Turtle()
